1d8b43db04e164b35cdb245e2c2922bdbb790ef5
Rhony <5601175+rhonyabdullah@users.noreply.github.com>
2025-03-26 16:33:37 +0700

    Multimedia: video

--- app/src/main/java/com/arch/multimedia/MainActivity.kt
+++ app/src/main/java/com/arch/multimedia/MainActivity.kt
@@ -2,14 +2,23 @@ package com.arch.multimedia
 
 import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,7 +28,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import androidx.compose.ui.viewinterop.AndroidView
 import com.arch.multimedia.ui.theme.ArchitectureMultimediaTheme
+import androidx.core.net.toUri
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +42,6 @@ class MainActivity : ComponentActivity() {
             ArchitectureMultimediaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -40,9 +51,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val absPath = context.externalCacheDir?.absolutePath
+    val videoView = VideoView(context)
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -77,6 +89,34 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         }) {
             Text("Play Audio")
         }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 12.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = {
+                val uri = "android.resource://${context.packageName}/${R.raw.emulator_mov}".toUri()
+                videoView.setVideoURI(uri)
+                videoView.setMediaController(MediaController(context))
+                videoView.requestFocus()
+                videoView.start()
+            }) {
+                Text("Play Video")
+            }
+
+            AndroidView(
+                modifier = Modifier
+                    .padding(horizontal = 4.dp)
+                    .weight(1f)
+                    .heightIn(max = 500.dp)
+                    .wrapContentHeight(),
+                factory = { context ->
+                    videoView
+                }
+            )
+        }
+
     }
 }
 
@@ -84,6 +124,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ArchitectureMultimediaTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
